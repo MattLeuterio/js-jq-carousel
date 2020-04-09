ec0e0cdda7f29b00462d7e9ceccd9880d6ba8aca
Matteo Francesco Leuterio <leuterio.matteo@gmail.com>
2020-04-09 17:58:57 +0200

    Added circle-section --> circle-active follow img-active (tidy)

--- js/main.js
+++ js/main.js
@@ -38,8 +38,27 @@ console.log('jQuery OK ->', $);
             $('.container.last-img').addClass('active');
         }
     });
+  
+    // Change img when click right or left arrow on keyboard 
+    $(document).keydown(function (param) {
+        if(param.keyCode == 39) {
+            if($('.container.active').hasClass('active') && $('.container.active').hasClass('last-img') == false) {
+                $('.container.active').removeClass('active').next().addClass('active') 
+            } else if ($('.container.active').hasClass('last-img')) {
+                $('.container.active').removeClass('active');
+                $('.container.first-img').addClass('active');
+            }
+        } else if(param.keyCode == 37) {
+            if($('.container.active').hasClass('active') && $('.container.active').hasClass('first-img') == false) {
+                $('.container.active').removeClass('active').prev().addClass('active') 
+            } else if ($('.container.active').hasClass('first-img')) {
+                $('.container.active').removeClass('active');
+                $('.container.last-img').addClass('active');
+            }
+        }
+     });
 
-    // Change Circle-Active when click arrows
+     // Change Circle-Active when click arrows
     rightArrLink.click( function () {
         if($('.circle.c-active').hasClass('c-active') && $('.circle.c-active').hasClass('last-circle') == false) {
             $('.circle.c-active').removeClass('c-active').next().addClass('c-active');
@@ -73,27 +92,6 @@ console.log('jQuery OK ->', $);
             } else if ($('.circle.c-active').hasClass('first-circle')) {
                 $('.circle.c-active').removeClass('c-active');
                 $('.circle.last-circle').addClass('c-active');
-            }
-        }
-     });
-
-
-    
-    // Change img when click right or left arrow on keyboard 
-    $(document).keydown(function (param) {
-        if(param.keyCode == 39) {
-            if($('.container.active').hasClass('active') && $('.container.active').hasClass('last-img') == false) {
-                $('.container.active').removeClass('active').next().addClass('active') 
-            } else if ($('.container.active').hasClass('last-img')) {
-                $('.container.active').removeClass('active');
-                $('.container.first-img').addClass('active');
-            }
-        } else if(param.keyCode == 37) {
-            if($('.container.active').hasClass('active') && $('.container.active').hasClass('first-img') == false) {
-                $('.container.active').removeClass('active').prev().addClass('active') 
-            } else if ($('.container.active').hasClass('first-img')) {
-                $('.container.active').removeClass('active');
-                $('.container.last-img').addClass('active');
             }
         }
      });
