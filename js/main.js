8cd6014aadb0ce3f52c462e81f37ce96e2f6e753
Matteo Francesco Leuterio <leuterio.matteo@gmail.com>
2020-04-09 17:57:23 +0200

    Added circle-section --> circle-active follow img-active

--- js/main.js
+++ js/main.js
@@ -22,38 +22,78 @@ console.log('jQuery OK ->', $);
     
     // Change Img when click arrows
     rightArrLink.click( function () {
-        if($('.carousel .container.active').hasClass('active') && $('.carousel .container.active').hasClass('last-img') == false) {
-            $('.carousel .container.active').removeClass('active').next().addClass('active') 
-        } else if ($('.carousel .container.active').hasClass('last-img')) {
-            $('.carousel .container.active').removeClass('active');
-            $('.carousel .container.first-img').addClass('active');
+        if($('.container.active').hasClass('active') && $('.container.active').hasClass('last-img') == false) {
+            $('.container.active').removeClass('active').next().addClass('active') 
+        } else if ($('.container.active').hasClass('last-img')) {
+            $('.container.active').removeClass('active');
+            $('.container.first-img').addClass('active');
         }
     }); 
 
     leftArrLink.click( function () {
-        if($('.carousel .container.active').hasClass('active') && $('.carousel .container.active').hasClass('first-img') == false) {
-            $('.carousel .container.active').removeClass('active').prev().addClass('active') 
-        } else if ($('.carousel .container.active').hasClass('first-img')) {
-            $('.carousel .container.active').removeClass('active');
-            $('.carousel .container.last-img').addClass('active');
+        if($('.container.active').hasClass('active') && $('.container.active').hasClass('first-img') == false) {
+            $('.container.active').removeClass('active').prev().addClass('active') 
+        } else if ($('.container.active').hasClass('first-img')) {
+            $('.container.active').removeClass('active');
+            $('.container.last-img').addClass('active');
         }
     });
+
+    // Change Circle-Active when click arrows
+    rightArrLink.click( function () {
+        if($('.circle.c-active').hasClass('c-active') && $('.circle.c-active').hasClass('last-circle') == false) {
+            $('.circle.c-active').removeClass('c-active').next().addClass('c-active');
+        }else if ($('.circle.c-active').hasClass('last-circle')) {
+            $('.circle.c-active').removeClass('c-active');
+            $('.circle.first-circle').addClass('c-active');
+        }
+    }); 
+
+    leftArrLink.click( function () {
+        if($('.circle.c-active').hasClass('c-active') && $('.circle.c-active').hasClass('first-circle') == false) {
+            $('.circle.c-active').removeClass('c-active').prev().addClass('c-active');
+        }else if ($('.circle.c-active').hasClass('first-circle')) {
+            $('.circle.c-active').removeClass('c-active');
+            $('.circle.last-circle').addClass('c-active');
+        }
+    }); 
+
+    // Change Circle-Active when click right or left arrow on keyboard 
+    $(document).keydown(function (param) {
+        if(param.keyCode == 39) {
+            if($('.circle.c-active').hasClass('c-active') && $('.circle.c-active').hasClass('last-circle') == false) {
+                $('.circle.c-active').removeClass('c-active').next().addClass('c-active') 
+            } else if ($('.circle.c-active').hasClass('last-circle')) {
+                $('.circle.c-active').removeClass('c-active');
+                $('.circle.first-circle').addClass('c-active');
+            }
+        } else if(param.keyCode == 37) {
+            if($('.circle.c-active').hasClass('c-active') && $('.circle.c-active').hasClass('first-circle') == false) {
+                $('.circle.c-active').removeClass('c-active').prev().addClass('c-active') 
+            } else if ($('.circle.c-active').hasClass('first-circle')) {
+                $('.circle.c-active').removeClass('c-active');
+                $('.circle.last-circle').addClass('c-active');
+            }
+        }
+     });
+
+
     
     // Change img when click right or left arrow on keyboard 
     $(document).keydown(function (param) {
         if(param.keyCode == 39) {
-            if($('.carousel .container.active').hasClass('active') && $('.carousel .container.active').hasClass('last-img') == false) {
-                $('.carousel .container.active').removeClass('active').next().addClass('active') 
-            } else if ($('.carousel .container.active').hasClass('last-img')) {
-                $('.carousel .container.active').removeClass('active');
-                $('.carousel .container.first-img').addClass('active');
+            if($('.container.active').hasClass('active') && $('.container.active').hasClass('last-img') == false) {
+                $('.container.active').removeClass('active').next().addClass('active') 
+            } else if ($('.container.active').hasClass('last-img')) {
+                $('.container.active').removeClass('active');
+                $('.container.first-img').addClass('active');
             }
         } else if(param.keyCode == 37) {
-            if($('.carousel .container.active').hasClass('active') && $('.carousel .container.active').hasClass('first-img') == false) {
-                $('.carousel .container.active').removeClass('active').prev().addClass('active') 
-            } else if ($('.carousel .container.active').hasClass('first-img')) {
-                $('.carousel .container.active').removeClass('active');
-                $('.carousel .container.last-img').addClass('active');
+            if($('.container.active').hasClass('active') && $('.container.active').hasClass('first-img') == false) {
+                $('.container.active').removeClass('active').prev().addClass('active') 
+            } else if ($('.container.active').hasClass('first-img')) {
+                $('.container.active').removeClass('active');
+                $('.container.last-img').addClass('active');
             }
         }
      });
